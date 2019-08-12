dccecdefac666c63f25d2ddc1ba3622dc00089b4
Ganesh Rajure <53991989+grajure@users.noreply.github.com>
2019-08-12 23:45:15 +0530

    Create Addition.java
    java two number addition program  .... second program

---
+++ Addition.java
@@ -0,0 +1,14 @@
+class Addition{
+	int a=2;
+	int b=3;
+	int c;
+public void add(){
+			c=a+b;
+    System.out.println("Addition of two number's is : "+c);
+		  }
+	public static void main(String args[]){
+						Addition c=new Addition();
+						c.add();
+
+						}
+		}
